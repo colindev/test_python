104b9eccb609e0ba6637016160c02656e1daaecb
colin <colin1124x@gmail.com>
2017-03-10 16:55:39 +0800

    .

--- setup.py
+++ setup.py
@@ -1,9 +1,12 @@
 from setuptools import setup, find_packages
 
 setup(
+    author='colin',
+    author_email='colin1124x@gmail.com',
     name='tester',
     version='0.0.0',
     packages=find_packages(),
+    url='https://github.com/colindev/test_python',
 
     classifiers = [
         'Programming Language :: Python :: 2.7'
